40c5d7a93ca11ca0baa6c433b2968adb53da331e
Dianashafee <diana.shafee1997@hotmail.com>
2020-09-07 14:25:27 +0300

    lab-02

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var userName = prompt('What is your name?')
+console.log(userName)
+alert('Welcome to my webpage  ' + userName)
+
+var feeling = prompt('Are you happy today ?')
+console.log(feeling);
+switch(feeling){
+    case 'yes'
+    alert('when you are happy, I am happy')
+ case 'no'
+
+ alert('Do not worry about anything, life is too short')
+}
+
+var 
